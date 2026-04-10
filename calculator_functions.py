e10341f1f9c07196f14736168390cfeb72ccad32
emy <emyjaneroyo332@gmail.com>
2026-04-10 12:20:17 +0800

    first commit

---
+++ calculator_functions.py
@@ -0,0 +1,6 @@
+def add_numbers(a, b):
+    return a + b
+num1_rrc = float(input("Enter first number: "))
+num2_rrc = float(input("Enter second number: "))
+result_rrc = add_numbers(num1_rrc, num2_rrc)
+print("Sum:", result_rrc)
